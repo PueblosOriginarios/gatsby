439af2bf515539b1c4766f174d50a78cfa98a1b0
erimasini2 <121623405+erimasini2@users.noreply.github.com>
2023-01-12 11:48:02 -0300

    fix pdf button

--- src/templates/Bibliografia.js
+++ src/templates/Bibliografia.js
@@ -1,82 +1,83 @@
-// import React from "react";
-// import { Page } from "../components/Page/index";
-// import { graphql } from "gatsby";
-// import SanityImage from "gatsby-plugin-sanity-image";
-// import { CustomSection } from "../components/index";
-// import "./Bibliografia.scss";
+import React from "react";
+import { Page} from "../components/index";
+import { graphql } from "gatsby";
+import "./Bibliografia.scss";
+import { PortableText } from "@portabletext/react";
+import Pdf from  "../components/Pdf/Pdf";
 
-// const BibliografiaPage = ({ data }) => {
- 
-//   const { title, imageHeader } = data?.allSanityBibliografia?.nodes[0];
-
-//   const pageInfo = data?.allSanityBibliografia?.nodes[0];
+const Bibliografia = ({ data }) => {
+    const { title, bibliografia } = data?.allSanityBibliografia?.nodes[0];
 
 
-//   return (
-//     <>
-//       <Page>
-//         <section className='article'>
-//           <div className='articleHeader'>
-//             <div className='empty-left'></div>
 
-//             <div className='mb-5'>
-//               {imageHeader && (
-//                 <SanityImage
-//                   {...imageHeader}
-//                   alt='Image Art'
-//                   className='imageHeader'
-//                 />
-//               )}
-
-//               <div className='titleContent'>
-//                 <div></div>
-//                 {title && (
-//                   <>
-//                     <h5 className='title'>{title}</h5>
-//                     <div className='vacio'></div>
-//                   </>
-//                 )}
-
-//                 <div></div>
-//               </div>
-//             </div>
-//             <div className='empty-right'></div>
-//           </div>
-//           <CustomSection sections={pageInfo?.BibliografiaBuilder} />
-//         </section>
-//       </Page>
-//     </>
-//   );
-// };
+    bibliografia.sort(function (a, b) {
+        if (a.author.toLowerCase() > b.author.toLowerCase()) {
+            return 1;
+        }
+        if (a.author.toLowerCase() < b.author.toLowerCase()) {
+            return -1;
+        }
+        return 0;
+    });
 
 
-// export default BibliografiaPage;
+    return (
+      
+          <>
+      <Page>
+        <section className='bibliografia'>
+          <div className='bibliografiaHeader'>
+            <div className='empty-left'></div>
 
-// export const query = graphql`
-//   query($slug: String!) {
-//     allSanityBibliografia(filter: { tipoBibliografia: { eq: $slug } }) {
-//       nodes {
-//         bibliografia {
-//             author
-//             richTextBody {
-//               _rawChildren
-//             }
-//             categoryReference {
-//               _key
-//               _type
-//               _rawCategoryReference
-//             }
-//             reference {
-//               categoryReferences {
-//                 _rawCategoryReference
-//                 categoryReference {
-//                   category
-//                 }
-//               }
-//             }
-//             title
-//           }
-//       }
-//     }
-//   }
-// `;
+            <div className='titleContent'>
+              <div></div>
+              {title && (
+                <>
+                  <h5 className='title'>{title}</h5>
+                  <div className='vacio'></div>
+                </>
+              )}
+            </div>
+            <div className='empty-right'></div>
+          </div>
+          {bibliografia.map((item, index) => (
+          <div>
+            <h5 className='title'>{item.title}</h5>
+            <p> Por {item.author}</p>
+            <PortableText value={item._rawRichTextBody} /> 
+            <Pdf data={item?.referenceBibliografia} />
+          </div>
+        ))}
+        </section>
+      </Page>
+    </>
+    );
+  };
+  
+  export default Bibliografia;
+
+
+  export const query = graphql`
+  query($slug: String!) {
+    allSanityBibliografia(filter: { tipoBibliografia: { eq: $slug } }) {
+      nodes {
+        title
+        bibliografia {
+          title
+          author
+          _rawRichTextBody
+          categoryReferenceBibliografia {
+            categoryReference {
+              category
+            }
+          }
+          referenceBibliografia {
+            tipoPdf
+            link
+          }
+        }
+        tipoBibliografia
+      }
+    }
+  }
+`;
