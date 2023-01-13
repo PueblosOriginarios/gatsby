a6010ddc2bb197549828e44ce557e158fdf8e3a9
Drina <drina.rincon@bitlogic.io>
2023-01-13 16:14:20 -0300

    add styles css

--- src/templates/Bibliografia.js
+++ src/templates/Bibliografia.js
@@ -1,63 +1,73 @@
 import React from "react";
-import { Page} from "../components/index";
+import { Page } from "../components/index";
 import { graphql } from "gatsby";
 import "./Bibliografia.scss";
 import { PortableText } from "@portabletext/react";
-import Pdf from  "../components/Pdf/Pdf";
+import Pdf from "../components/Pdf/Pdf";
 
 const Bibliografia = ({ data }) => {
-    const { title } = data?.allSanityBibliografia?.nodes[0];
-    const  bibliografia = data?.allSanityComponentbibliografia?.nodes;
-  
+  const { title } = data?.allSanityBibliografia?.nodes[0];
+  const bibliografia = data?.allSanityComponentbibliografia?.nodes;
 
-    bibliografia.sort(function (a, b) {
-        if (a.author.toLowerCase() > b.author.toLowerCase()) {
-            return 1;
-        }
-        if (a.author.toLowerCase() < b.author.toLowerCase()) {
-          return -1;
-        }
-        return 0;
-    });
-    
+  bibliografia.sort(function (a, b) {
+    if (a.author.toLowerCase() > b.author.toLowerCase()) {
+      return 1;
+    }
+    if (a.author.toLowerCase() < b.author.toLowerCase()) {
+      return -1;
+    }
+    return 0;
+  });
 
-    return (
-      
-          <>
+  return (
+    <>
       <Page>
-        { <section className='bibliografia'>
-          <div className='bibliografiaHeader'>
-            <div className='empty-left'></div>
+        {
+          <section className='bibliografia'>
+            <div className='bibliografiaHeader'>
+              <div className='empty-left'></div>
 
-            <div className='titleContent'>
-              <div></div>
-              {title && (
-                <>
-                  <h5 className='title'>{title}</h5>
-                  <div className='vacio'></div>
-                </>
-              )}
+              <div className='titleContent mb-4'>
+                <div></div>
+                {title && (
+                  <>
+                    <h5 className='title'>{title}</h5>
+                    <div className='vacio'></div>
+                  </>
+                )}
+                <div></div>
+              </div>
+              <div className='empty-right'></div>
             </div>
-            <div className='empty-right'></div>
-          </div>
-          {bibliografia.map((item, index) => (
-          <div>
-            <h5 className='title'>{item.title}</h5>
-            <p> Por {item.author}</p>
-            <PortableText value={item._rawRichTextBody} /> 
-            <Pdf data={item?.referenceBibliografia} />
-          </div>
-        ))}
-        </section>}
+
+            {bibliografia.map((item, index) => (
+              <div className='contentBibliografia' key={index}>
+                <div className='empty-left'></div>
+                <div className='textBibliografia'>
+                  <div></div>
+                  <div>
+                    <h5 className='title'>{item.title}</h5>
+                    <p> Por {item.author}</p>
+                    <PortableText value={item._rawRichTextBody} />
+                    <Pdf data={item?.referenceBibliografia} />
+                    <div className='line'></div>
+                  </div>
+
+                  <div></div>
+                </div>
+                <div className='empty-right'></div>
+              </div>
+            ))}
+          </section>
+        }
       </Page>
     </>
-    );
-  };
-  
-  export default Bibliografia;
+  );
+};
 
+export default Bibliografia;
 
-  export const query = graphql`
+export const query = graphql`
   query($slug: String!) {
     allSanityBibliografia(filter: { tipoBibliografia: { eq: $slug } }) {
       nodes {
@@ -66,7 +76,9 @@ const Bibliografia = ({ data }) => {
       }
     }
 
-    allSanityComponentbibliografia(filter: { filtroBilbliografia:{  in: [$slug, "ambos"]  } }) {
+    allSanityComponentbibliografia(
+      filter: { filtroBilbliografia: { in: [$slug, "ambos"] } }
+    ) {
       nodes {
         author
         title
@@ -75,9 +87,8 @@ const Bibliografia = ({ data }) => {
         referenceBibliografia {
           link
           tipoPdf
-          }
         }
+      }
     }
-
   }
 `;
